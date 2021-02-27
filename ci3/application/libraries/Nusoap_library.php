15d92f7531c17fe82bb820f0d72bfdfa7e4ba5c3
ekacandrika <ekacandrika@gmail.com>
2021-02-27 13:45:36 +0700

    clear repo

--- ci3/application/libraries/Nusoap_library.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Nusoap_library{
-	function Nusoap_library(){
-		require_once('lib/nusoap.php');
-	}
-}
-?>
-
--- get_path.php
+++
@@ -1,6 +0,0 @@
-<?php
-// print_r(get_loaded_extensions());
-session_start();
-$_SESSION['color']='red';
-echo "the color".$_SESSION['color'];
-?>
